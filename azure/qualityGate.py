0f23d1b1f09368d0ce989b9559b33c3986e5b0b4
Rob Jahn <rob.jahn@gmail.com>
2019-02-01 07:22:15 -0500

    Adjust argument processing

--- azure/qualityGate.py
+++ azure/qualityGate.py
@@ -6,15 +6,19 @@ if len(sys.argv)-1 == 0:
     
 print('')
 print('====================================================')
-print('Gate Test')
+print('Parse Arguments')
 print('====================================================')
+print('Number of args:' + str(len(sys.argv)-1))
 print('arg 1:', sys.argv[1])
-if len(sys.argv)-1 == 2:
+if len(sys.argv)-1 >= 2:
     print('arg 2:' + sys.argv[2])
 
-if len(sys.argv)-1 == 3:
-    print('arg 2:' + sys.argv[3])
+if len(sys.argv)-1 >= 3:
+    print('arg 3:' + sys.argv[3])
 
+print('====================================================')
+print('Gate Test')
+print('====================================================')
 try:
     if sys.argv[1] == 'pass':
         print('I passed')
