9bb6a6cb507f4de357225c9feafb6162136eb383
Rob Jahn <robjahn@gmail.com>
2019-02-01 07:06:54 -0500

    adding printing of optional args

--- azure/qualityGate.py
+++ azure/qualityGate.py
@@ -3,12 +3,18 @@ import sys
 if len(sys.argv)-1 == 0:
     print('Missing argument. Value of pass will return pass. Any other will return fail.')
     exit(1)
-
+    
 print('')
 print('====================================================')
 print('Gate Test')
 print('====================================================')
-print('Argument Value:', sys.argv[1])
+print('arg 1:', sys.argv[1])
+if len(sys.argv)-1 == 2:
+    print('arg 2:' + sys.argv[2])
+
+if len(sys.argv)-1 == 3:
+    print('arg 2:' + sys.argv[3])
+
 try:
     if sys.argv[1] == 'pass':
         print('I passed')
